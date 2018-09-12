dce52ba8057f5e44da43559489b4159273528607
Gumkle <dziedzicdev@gmail.com>
2018-09-12 06:26:56 +0200

    Optimalistion

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
-/**
- * Losowanie słowa
- */
-function randomWord(){
-    const lang          = process.argv[3];
-    const langPack      = require('./langs/'+lang+'.json');
-    const pl            = Object.values(langPack['words']);
-    const niepl         = Object.keys(langPack['words']);
-    const randomIndex   = Math.floor(Math.random()*pl.length);
-
-    console.log(pl[randomIndex]);
-
-    process.stdin.on('data', () => {
-        console.log(niepl[randomIndex]);
-        process.exit(1);
-    });
-}
-
-/**
- * Losowanie zdania
- */
-function randomSentence(){
-    const lang      = process.argv[3];
-    const langPack  = require('./langs/'+lang+'.json');
-    const pl            = Object.values(langPack['sentences']);
-    const niepl         = Object.keys(langPack['sentences']);
-    const randomIndex   = Math.floor(Math.random()*pl.length);
-
-    console.log(pl[randomIndex]);
-
-    process.stdin.on('data', () => {
-        console.log(niepl[randomIndex]);
-        process.exit(1);
-    });
-}
+'use strict';
 
 /**
  * Inicjalizacja, decyzja o wywolaniu okreslonej funkcji
  */
 function init(){
-    const mode = process.argv[2];
-    switch(mode){
-        case 'word':
-            randomWord();
-            break;
-        case 'sentence':
-            randomSentence();
-            break;
-        default:
-            throw new Error('Nieznane polecenie');
-    }
+    const mode          = process.argv[2];
+    const lang          = process.argv[3];
+    const langPack      = require('./langs/'+lang+'.json');
+    const niepl         = Object.values(langPack[mode]);
+    const pl            = Object.keys(langPack[mode]);
+    const randomIndex   = Math.floor(Math.random()*niepl.length);
+
+    console.log(niepl[randomIndex]);
+
+    process.stdin.on('data', () => {
+        console.log(pl[randomIndex]);
+        process.exit(1);
+    });
 }
 
 module.exports.init = init();
